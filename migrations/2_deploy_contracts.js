71360218e33f074011f8996d058aa14e181e7e99
Sean Thoman <sean@sthoman.com>
2018-11-17 15:09:26 -0500

    Fix migrations

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,7 +3,9 @@ var PropertyCoin = artifacts.require("./PropertyCoin.sol");
 var PropertyRegistry = artifacts.require("./PropertyRegistry.sol");
 
 module.exports = function(deployer) {
-    deployer.deploy(PropertyCoin);
-    deployer.deploy(Property, 'Property', 'PROP');
-    deployer.deploy(PropertyRegistry, '');
+    deployer.deploy(Property, 'Property', 'PROP').then(
+      () => {
+        deployer.deploy(PropertyCoin);
+        deployer.deploy(PropertyRegistry, Property.address);
+    });
 };
