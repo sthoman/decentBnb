88f3e2ec0ab300cab3cf08945f0b5c9ee9155230
Sean Thoman <sean@sthoman.com>
2018-11-18 21:03:48 -0500

    PropertyToken minting tests

--- contracts/PropertyToken.sol
+++ contracts/PropertyToken.sol
@@ -7,7 +7,5 @@ contract PropertyToken is DetailedERC20, MintableToken {
 
   //set up the property contract as minimum interface to prove ownership ERC721Basic
   constructor(string _name, string _symbol, uint8 _decimals) public DetailedERC20(_name, _symbol, _decimals) {
-
   }
-
 }
--- test/BnbTest.js
+++ test/BnbTest.js
@@ -2,21 +2,28 @@ const Property = artifacts.require("./Property.sol");
 const PropertyToken = artifacts.require("./PropertyToken.sol");
 const PropertyRegistry = artifacts.require("./PropertyRegistry.sol");
 
-/*
-contract('PropertyCoin Contract Tests', function() {
-  it('should be deployed, PropertyCoin', async () => {
-    propertyCoin = await PropertyCoin.deployed()
-    assert(propertyCoin !== undefined, 'PropertyCoin deployment failed')
-  });
-});
-*/
 contract('PropertyRegistry Contract Tests', accounts => {
 
-  bob = accounts[0];
-  alice = accounts[1];
+  alice = accounts[0];
+  bob = accounts[1];
   registry_address = '';
   contract_address = '';
+  token_address = '';
+  allocation = 10000;
+  _propertyToken = undefined;
   _propertyRegistry = undefined;
+
+  it('should be deployed, PropertyToken', async () => {
+    _propertyToken = await PropertyToken.deployed()
+    token_address = _propertyToken.address;
+    assert(_propertyToken !== undefined, 'PropertyToken deployment failed')
+  });
+
+  it('should be able to mint, PropertyToken', async () => {
+    const tx = await _propertyToken.mint(bob, allocation);
+    const balance = await _propertyToken.balanceOf.call(bob);
+    assert.equal(balance.toNumber(), allocation, 'balance');
+  });
 
   it('should be deployed, Property', async () => {
     property = await Property.deployed()
@@ -45,10 +52,18 @@ contract('PropertyRegistry Contract Tests', accounts => {
     await _propertyRegistry.registerProperty(1, 100000, 'https://');
     assert(_propertyRegistry.propertyDetails !== undefined, 'PropertyRegistry registration failed');
   });
+
+  it('should allow bob to approve the property registry to use his tokens', async () => {
+    const tx = await _propertyToken.approve(_propertyRegistry.address, 1000, { from: bob });
+    console.log(tx);
+    assert(tx !== undefined, 'property has not been approved');
+  });
+
 /*
   it('should take a request from Bob, PropertyRegistry', async () => {
     await _propertyRegistry.requestStay(property.address, { from: bob });
     assert(_propertyRegistry.propertyDetails.length == 1, 'PropertyRegistry took a request from Bob')
   });
 */
+
 });
